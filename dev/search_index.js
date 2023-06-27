fd63ce486be883ccfd24b588a5a9fb2c480d7177
Documenter.jl <documenter@juliadocs.github.io>
2023-06-27 15:46:40 +0000

    build based on 34e133b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BlankLocalizationCore","category":"page"},{"location":"#BlankLocalizationCore","page":"Home","title":"BlankLocalizationCore","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BlankLocalizationCore.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BlankLocalizationCore]","category":"page"}]
+}
